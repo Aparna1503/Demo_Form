108d7d0947788b878fdb78b9ab0df8585a5dfbf7
Aparna1503 <74192394+Aparna1503@users.noreply.github.com>
2025-02-14 11:53:46 +0530

    Update Form1.java

--- Demo_Form/src/test/java/Demo/Form1.java
+++ Demo_Form/src/test/java/Demo/Form1.java
@@ -18,64 +18,44 @@ public class Form1 {
 		 driver.findElement(By.xpath("//input[@id='g4072-fullname']")).click();
 		 
 		 driver.findElement(By.xpath("//input[@id='g4072-fullname']")).sendKeys("Aparna");
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-email']")).sendKeys("aprna12345@gamil.com");
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-phonenumber']")).sendKeys("7083573291");
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//select[@id='g4072-gender']")).sendKeys("Female");
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-1']")).click();
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-2']")).click();
-		 
+	         Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-3']")).click();
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-4']")).click();
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-5']")).click();
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-Above 5']")).click();
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-skills-Functional testing']")).click();
-		 
-		 
+                 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-skills-Automation testing']")).click();
-		 
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-skills-API testing']")).click();
-		 
-		 
+		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@id='g4072-skills-DB testing']")).click();
-		 
 		 Thread.sleep(2000);
-		 
 		 driver.findElement(By.xpath("//select[@id='g4072-qatools']")).sendKeys("Selenium");
-		 
 		 Thread.sleep(2000);
-		 
 		 driver.findElement(By.xpath("//select[@id='g4072-qatools']")).sendKeys("Appium");
-		 
-
 		 Thread.sleep(2000);
-		 
 		 driver.findElement(By.xpath("//select[@id='g4072-qatools']")).sendKeys("Cypress");
-
 		 Thread.sleep(2000);
-		
 		 driver.findElement(By.xpath("//select[@id='g4072-qatools']")).sendKeys("protractor");
-
-		 Thread.sleep(2000);
-		
-		 driver.findElement(By.xpath("//textarea[@id='contact-form-comment-g4072-otherdetails']")).sendKeys("hello,My Name is Aparna.");
+		 Thread.sleep(2000)
+		 driver.findElement(By.xpath("//textarea[@id='contact-form-comment-g4072-otherdetails']")).sendKeys("hello,My Name is Aparna."); 
 		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
+	       } 
 	}
 
-}
+
