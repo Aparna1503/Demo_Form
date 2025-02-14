813b59737b50268c7f9b9364bb175a13d11ee2a8
GITINIPANI <GITINIPANI@Shubham>
2025-02-14 11:49:56 +0530

    initial commit

---
+++ Demo_Form/src/test/java/Demo/Form1.java
@@ -0,0 +1,81 @@
+package Demo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Form1 {
+	@Test
+	public void Amethode() throws Exception {
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://qavalidation.com/demo-form/");
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-fullname']")).click();
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-fullname']")).sendKeys("Aparna");
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-email']")).sendKeys("aprna12345@gamil.com");
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-phonenumber']")).sendKeys("7083573291");
+		 
+		 driver.findElement(By.xpath("//select[@id='g4072-gender']")).sendKeys("Female");
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-1']")).click();
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-2']")).click();
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-3']")).click();
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-4']")).click();
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-5']")).click();
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-yearsofexperience-Above 5']")).click();
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-skills-Functional testing']")).click();
+		 
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-skills-Automation testing']")).click();
+		 
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-skills-API testing']")).click();
+		 
+		 
+		 driver.findElement(By.xpath("//input[@id='g4072-skills-DB testing']")).click();
+		 
+		 Thread.sleep(2000);
+		 
+		 driver.findElement(By.xpath("//select[@id='g4072-qatools']")).sendKeys("Selenium");
+		 
+		 Thread.sleep(2000);
+		 
+		 driver.findElement(By.xpath("//select[@id='g4072-qatools']")).sendKeys("Appium");
+		 
+
+		 Thread.sleep(2000);
+		 
+		 driver.findElement(By.xpath("//select[@id='g4072-qatools']")).sendKeys("Cypress");
+
+		 Thread.sleep(2000);
+		
+		 driver.findElement(By.xpath("//select[@id='g4072-qatools']")).sendKeys("protractor");
+
+		 Thread.sleep(2000);
+		
+		 driver.findElement(By.xpath("//textarea[@id='contact-form-comment-g4072-otherdetails']")).sendKeys("hello,My Name is Aparna.");
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	}
+
+}
